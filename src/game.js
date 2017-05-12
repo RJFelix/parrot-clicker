6b9de62f43e726f560a0cebdd89c27e819700ec4
R F <a>
2017-05-11 22:58:29 -0700

    add modal to confirm restart game

--- src/game.js
+++ src/game.js
@@ -248,9 +248,11 @@ function Game() {
   }
 }
 
+// Setup code.
 const game = new Game();
 game.start();
 
+// Click handlers
 function squawkClick() {
   game.addSquawks(1);
 }
@@ -259,6 +261,58 @@ function parrotClick(parrot) {
   game.addParrots(parrot, 1);
 }
 
+/*
+   SUPER IMPORTANT NOTE!
+
+   I wouldn't actually implement a modal this way,
+   I'd just toss it in the initial HTML and toggle
+   an 'is-visible' class to show/hide it.
+   
+   I did it this way because I wasn't sure if you
+   needed to see DOM traversal + node creation/removal,
+   so I contrived a simple example to cover all
+   of the bases :)
+
+*/
+
 function restartClick() {
-  game.restart();
+
+  const modalBackground = document.createElement("div");
+  modalBackground.classList.add("modal-background");
+
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  modal.appendChild(
+    document.createTextNode("Are you sure you want to restart? You will lose your progress!")
+  );
+
+  const removeModal = () => {
+    const body = document.querySelector("body");
+    body.removeChild(modal);
+    body.removeChild(modalBackground);
+  }
+
+  const yesButton = document.createElement("button");
+  yesButton.appendChild(document.createTextNode("Yes"));
+  yesButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    game.restart();
+    removeModal();
+  })
+
+  const noButton = document.createElement("button");
+  noButton.appendChild(document.createTextNode("No"));
+  noButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    removeModal();
+  })
+
+  const modalInnerDiv = document.createElement("div");
+  modalInnerDiv.appendChild(yesButton);
+  modalInnerDiv.appendChild(noButton);
+  modal.appendChild(modalInnerDiv);
+
+  
+  document.querySelector("body").appendChild(modalBackground);
+  document.querySelector("body").appendChild(modal);
 }
